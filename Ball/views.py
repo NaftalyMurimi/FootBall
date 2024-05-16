4d6c4905ca758efdd6e3e05e01a1708f8f9891fb
NaflayM <nafmurimi@gmail.com>
2024-05-16 19:45:50 +0300

    failed requests

--- Ball/views.py
+++ Ball/views.py
@@ -1,7 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import json
+import serpapi
+import os
+from dotenv import load_dotenv
+import serpapi.client
+load_dotenv()
+api = os.getenv('key')
 
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
-    # return HttpResponse("You all Time Football")
+    client = serpapi.Client(api_key = api)
+    results = client.search(
+        q="coffee",
+        engine = "google",
+        location = "nairobi, kenya",
+        hl = "en",
+        gl = "us",
+        num = "1"
+    )
+    results = results.json()
+    organic_results = results["organic_results"]
+# print(results)
+    # for item in results["organic_results"]:
+    #     print(item['title'])
+    #     print(item['link'])
+    #     print(item['snippet'])
+    #     print("------------------------------------------")
+    return render(request=request,
+                   template_name='index.html',
+                   context= {"organic_results": organic_results})
+        # return HttpResponse("You all Time Football")
