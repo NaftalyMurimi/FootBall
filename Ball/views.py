047f4c66f91b92b9cafe7587f07749d8f8c98e72
NaflayM <nafmurimi@gmail.com>
2024-05-20 14:23:17 +0300

    serpapi working

--- Ball/views.py
+++ Ball/views.py
@@ -1,33 +1,37 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
-import serpapi
-import os
+from serpapi import GoogleSearch
 from dotenv import load_dotenv
 import serpapi.client
 load_dotenv()
+import os
 api = os.getenv('key')
 
-# Create your views here.
 def index(request):
-    client = serpapi.Client(api_key = api)
-    results = client.search(
-        q="coffee",
-        engine = "google",
-        location = "nairobi, kenya",
-        hl = "en",
-        gl = "us",
-        num = "1"
-    )
-    #results = results.json()
+    params = {
+        "engine": "google",
+        "q": "man u vs brighton",
+        "location": "Seattle-Tacoma, WA, Washington, United States",
+        "hl": "en",
+        "gl": "us",
+        "google_domain": "google.com",
+        "num": "5",
+        "start": "10",
+        "safe": "active",
+        "api_key": api
+    }
+
+    search = GoogleSearch(params)
+    results = search.get_dict()
     organic_results = results["organic_results"]
-# print(results)
+    #print(organic_results)
+    #print(results)
     # for item in results["organic_results"]:
     #     print(item['title'])
     #     print(item['link'])
     #     print(item['snippet'])
     #     print("------------------------------------------")
+
     return render(request=request,
                    template_name='index.html',
                    context= {"organic_results": organic_results})
-        # return HttpResponse("You all Time Football")
--- Ball/well.py
+++ Ball/well.py
@@ -1,19 +1,28 @@
-import serpapi
-import os
+from serpapi import GoogleSearch
 from dotenv import load_dotenv
 import serpapi.client
 load_dotenv()
+import os
 api = os.getenv('key')
-client = serpapi.Client(api_key = api)
-results = client.search(
-     q="coffee",
-    engine = "google",
-    location = "nairobi, kenya",
-    hl = "en",
-    gl = "us",
-    num = "5"
-)
-# print(results)
+
+params = {
+  "engine": "google",
+  "q": "man u",
+  "location": "Seattle-Tacoma, WA, Washington, United States",
+  "hl": "en",
+  "gl": "us",
+  "google_domain": "google.com",
+  "num": "5",
+  "start": "10",
+  "safe": "active",
+  "api_key": api
+}
+
+search = GoogleSearch(params)
+results = search.get_dict()
+organic_results = results["organic_results"]
+print(organic_results)
+print(results)
 for item in results["organic_results"]:
     print(item['title'])
     print(item['link'])
