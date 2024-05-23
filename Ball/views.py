6798705b0205594773efba58e2836341294f1d4b
NaflayM <nafmurimi@gmail.com>
2024-05-23 14:47:46 +0300

    extends base template

--- Ball/views.py
+++ Ball/views.py
@@ -22,41 +22,41 @@ from .forms import MessageForm
 
 # views.py
 def index(request):
-    if request.method == 'POST':
-        form = MessageForm(request.POST)
-        if form.is_valid():
-            message = form.cleaned_data['message']
-            address = form.cleaned_data['address']
-            # params = {
-            #     "engine": "google",
-            #     "q": "Coffee",
-            #     "location": "Seattle-Tacoma, WA, Washington, United States",
-            #     "hl": "en",
-            #     "gl": "us",
-            #     "google_domain": "google.com",
-            #     "num": "4",
-            #     "start": "1",
-            #     "safe": "active",
-            #     "api_key": api
-            # }
+    # if request.method == 'POST':
+    #     form = MessageForm(request.POST)
+    #     if form.is_valid():
+    #         message = form.cleaned_data['message']
+    #         address = form.cleaned_data['address']
+            params = {
+                "engine": "google",
+                "q": "Coffee",
+                "location": "Seattle-Tacoma, WA, Washington, United States",
+                "hl": "en",
+                "gl": "us",
+                "google_domain": "google.com",
+                "num": "4",
+                "start": "1",
+                "safe": "active",
+                "api_key": api
+            }
 
-            # search = GoogleSearch(params)
-            # results = search.get_dict()
-            #organic_results = results["organic_results"]
-                    #print(organic_results)
-                    #print(results)
-                    # for item in results["organic_results"]:
-                    #     print(item['title'])
-                    #     print(item['link'])
-                    #     print(item['snippet'])
-                    #     print("------------------------------------------")
-            return render(request, 'index.html', {'message': message, 'address': address})
-            # return render(request=request,
-            #                     template_name='index.html',
-            #                     context= {"organic_results": organic_results})
-    else:
-        form = MessageForm()
-    return render(request, 'index.html', {'form': form})
+            search = GoogleSearch(params)
+            results = search.get_dict()
+            organic_results = results["organic_results"]
+            print(organic_results)
+            print(results)
+            for item in results["organic_results"]:
+                print(item['title'])
+                print(item['link'])
+                print(item['snippet'])
+                print("------------------------------------------")
+            #return render(request, 'index.html', {'message': message, 'address': address})
+            return render(request=request,
+                                template_name='index.html',
+                                context= {"organic_results": organic_results})
+    # else:
+    #     form = MessageForm()
+    # return render(request, 'index.html', {'form': form})
 
 
 def age(request):
